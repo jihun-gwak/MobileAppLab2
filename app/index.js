9e5bbcee35367716551253ff267f770584fbd6c9
Jihun Gwak <gjihun1395@gmail.com>
2024-10-30 18:10:26 -0600

    lab 3

--- app/index.js
+++ app/index.js
@@ -1,12 +1,17 @@
 import { View } from "react-native";
 import ToDoList from "./ToDoList";
 import ToDoForm from "./toDoForm";
+import { useState } from "react";
 
 export default function Page() {
+  const toDoList = ["Do laundry", "Go to gym", "Walk dog"];
+
+  const [tasks, setTasks] = useState(toDoList);
+
   return (
     <View>
-      <ToDoList></ToDoList>
-      <ToDoForm></ToDoForm>
+      <ToDoList tasks={tasks} />
+      <ToDoForm />
     </View>
   );
 }
